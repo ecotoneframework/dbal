81205707ea11ae6eb6f62c8158feab9ea1b95f8e
Serg Lifinsky <serg.lifinsky@gmail.com>
2022-08-16 01:48:48 +0300

    Update DbalConnection class

--- src/DbalConnection.php
+++ src/DbalConnection.php
@@ -37,41 +37,65 @@ class DbalConnection implements ManagerRegistry
         return new ManagerRegistryConnectionFactory($managerRegistry, ['connection_name' => $connectionName]);
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getDefaultConnectionName(): string
     {
         return 'default';
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getConnection($name = null): object
     {
         return $this->connection;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getConnections(): array
     {
         return [$this->connection];
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getConnectionNames(): array
     {
         return ['default'];
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getDefaultManagerName(): string
     {
         return 'default';
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getManager($name = null): ObjectManager
     {
         return $this->entityManager;
     }
 
-    public function getManagers()
+    /**
+     * {@inheritdoc}
+     */
+    public function getManagers(): array
     {
         return $this->entityManager ? [$this->entityManager] : [];
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function resetManager($name = null): ObjectManager
     {
         $this->entityManager->getUnitOfWork()->clear();
@@ -79,21 +103,33 @@ class DbalConnection implements ManagerRegistry
         return $this->entityManager;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getAliasNamespace($alias): string
     {
         throw InvalidArgumentException::create('Method not supported');
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getManagerNames(): array
     {
         return ['default'];
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getRepository($persistentObject, $persistentManagerName = null): ObjectRepository
     {
         return $this->entityManager->getRepository($persistentObject);
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getManagerForClass($class): ?ObjectManager
     {
         return $this->entityManager;
