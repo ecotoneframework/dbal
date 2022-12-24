e063c5a209a380ab477aff41477cdecc8605ee7c
Dariusz <dgafka.mail@gmail.com>
2022-12-24 11:37:53 +0000

    open 1.63-dev

---
+++ src/DbalInboundChannelAdapterBuilder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Ecotone\Dbal;
+
+use Ecotone\Enqueue\CachedConnectionFactory;
+use Ecotone\Enqueue\EnqueueInboundChannelAdapterBuilder;
+use Ecotone\Enqueue\InboundMessageConverter;
+use Ecotone\Messaging\Conversion\ConversionService;
+use Ecotone\Messaging\Endpoint\PollingMetadata;
+use Ecotone\Messaging\Handler\ChannelResolver;
+use Ecotone\Messaging\Handler\ReferenceSearchService;
+use Ecotone\Messaging\MessageConverter\DefaultHeaderMapper;
+use Enqueue\Dbal\DbalConnectionFactory;
+
+class DbalInboundChannelAdapterBuilder extends EnqueueInboundChannelAdapterBuilder
+{
+    public static function createWith(string $endpointId, string $queueName, ?string $requestChannelName, string $connectionReferenceName = DbalConnectionFactory::class): self
+    {
+        return new self($queueName, $endpointId, $requestChannelName, $connectionReferenceName);
+    }
+
+    public function createInboundChannelAdapter(ChannelResolver $channelResolver, ReferenceSearchService $referenceSearchService, PollingMetadata $pollingMetadata): DbalInboundChannelAdapter
+    {
+        /** @var DbalConnection $connectionFactory */
+        $connectionFactory = $referenceSearchService->get($this->connectionReferenceName);
+        /** @var ConversionService $conversionService */
+        $conversionService = $referenceSearchService->get(ConversionService::REFERENCE_NAME);
+
+        $headerMapper = DefaultHeaderMapper::createWith($this->headerMapper, [], $conversionService);
+
+        return new DbalInboundChannelAdapter(
+            CachedConnectionFactory::createFor(new DbalReconnectableConnectionFactory($connectionFactory)),
+            $this->buildGatewayFor($referenceSearchService, $channelResolver, $pollingMetadata),
+            $this->declareOnStartup,
+            $this->messageChannelName,
+            $this->receiveTimeoutInMilliseconds,
+            new InboundMessageConverter($this->getEndpointId(), $this->acknowledgeMode, $headerMapper)
+        );
+    }
+}
---
+++ src/Recoverability/DeadLetterGateway.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Ecotone\Dbal\Recoverability;
+
+use Ecotone\Messaging\Handler\Recoverability\ErrorContext;
+use Ecotone\Messaging\Message;
+
+interface DeadLetterGateway
+{
+    /**
+     * @return ErrorContext[]
+     */
+    public function list(int $limit, int $offset): array;
+
+    public function show(string $messageId): Message;
+
+    public function count(): int;
+
+    public function reply(string|array $messageId): void;
+
+    public function replyAll(): void;
+
+    public function delete(string|array $messageId): void;
+}
